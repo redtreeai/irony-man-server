c89aaf80edd7b8299697d7151453302f8515a7b7
陈鸿澍 <redtree@aliyun.com>
2019-12-18 17:45:20 +0800

    udpate

--- service/wechat/code2session_manager.py
+++ service/wechat/code2session_manager.py
@@ -15,17 +15,22 @@ from utils.http import responser
 def check_login(code):
     #WECHAT_APPID='wx0e4041630f2f5e83'
     #WECHAT_APPSECRET= 'ccab6c1b52643354323e763f60f8a869'
-    url = 'https://api.weixin.qq.com/sns/jscode2session?appid='+WECHAT_APPID+'&secret='+WECHAT_APPSECRET+'&js_code='+code+'&grant_type=authorization_code'
-    r = requests.get(url)
-    res = json.loads(r.text)
-    errcode = res['errcode']
+    try:
+        url = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + WECHAT_APPID + '&secret=' + WECHAT_APPSECRET + '&js_code=' + code + '&grant_type=authorization_code'
+        r = requests.get(url)
+        res = json.loads(r.text)
+        print(res)
+        errcode = res['errcode']
 
-    if errcode == 0 :
-        openid = res['openid']
-        session_key = res['session_key']
-        unionid = res['unionid']
-        errmsg = res['errmsg']
-        return responser.send(10000,'登录成功')
-    else:
+        if errcode == 0:
+            openid = res['openid']
+            session_key = res['session_key']
+            unionid = res['unionid']
+            errmsg = res['errmsg']
+            return responser.send(10000, '登录成功')
+        else:
+            return responser.send(80001)
+    except:
         return responser.send(80001)
+
 
